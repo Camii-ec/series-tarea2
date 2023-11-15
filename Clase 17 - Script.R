d137270c9234cd3318c9180537de96a0a6d81023
fvilca <fvilca@uc.cl>
2023-11-14 22:51:40 -0300

    aaaaaaa

---
+++ Clase 17 - Script.R
@@ -0,0 +1,148 @@
+#####################
+## Script Clase 17 ##
+#####################
+
+## Ejemplo: 
+X <- LSTS::malleco
+
+par(mfrow = c(1,1), bty = "n", las = 1)
+plot(X, xlim = c(1200, 2000), ylim = c(0,2), xlab = "", ylab = "", lwd = 2)
+acf(X, lag.max = 10, ylim = c(-1,+1), main = "", lwd = 3)
+
+## 4. Algotirmo de Durbin - Levinson (EMV)
+
+source("Durbin_Levinson.R")
+
+## Ej: LSTS::malleco
+
+X <- LSTS::malleco
+n <- length(X)
+plot(X)
+par(mfrow = c(1,2))
+ACF <- acf(X, ylim = c(-1,+1), lag.max = 10)
+pacf(X, ylim = c(-1,+1), lag.max = 10, xlim = c(0,10))
+
+## Estimación de Máxima Verosimilitud
+
+## Estimador de Máxima Verosimilitud (Durbin Levinson)
+dl.loglik = function(x, serie, p = 1, q = 1){
+  ar = NULL
+  ma = NULL 
+  if(p > 0){
+    ar  = x[1:p]
+  }
+  if(q > 0){
+    ma  = x[(p+1):(p+q)]
+  }
+  fit   = DurbinLevinson(serie = serie, ar = ar, ma = ma)
+  e     = serie-fit$fitted
+  nu    = fit$nu 
+  aux = 0.5*(sum(log(fit$nu)) +  sum(e^2/fit$nu))
+  if(is.nan(aux)){aux = Inf}
+  aux
+}
+
+fit <- nlminb(start = c(0.8), objective = dl.loglik, serie = X-mean(X), p = 1, q = 0, lower = c(-0.99999), upper = c(+0.99999))
+fit
+
+############################################
+## Método de Máxima Verosimilitid Whittle ##
+############################################
+
+whittle.loglik = function(x, serie, p = 1, q = 1){
+  Y = serie
+  n = length(Y)
+  ar  = x[1:p]
+  ma  = x[(p+1):(p+q)]
+  if(p == 0){ar = numeric()}
+  if(q == 0){ma = numeric()}
+  sigma = x[p+q+1]
+  aux = LSTS::periodogram(Y, plot = F)
+  I = aux$periodogram
+  lambda = aux$lambda
+  f = LSTS::spectral.density(ar = ar, ma = ma, sd = sigma, lambda = lambda)
+  aux = 0.5*(sum(log(f)) + sum(I/f))/n
+  aux
+}
+
+nlminb(start = c(0.5, 1), objective = whittle.loglik, serie = X, p = 1, q = 0)$par
+
+
+############################
+## Inferencia Estadística ##
+############################
+
+## Gamma teorica para AR(1):
+f = function(x,phi){
+  ((-2*cos(x)+2*phi)/(1-2*phi*cos(x)+phi^2))^2/(4*pi)
+}
+
+integrate(f, lower = -pi, upper = +pi, phi = 0.5)$value
+phi = 0.5
+1/(1-phi^2)
+
+## Gamma teórica para ARMA(1,1)
+
+var.coef.arma11 <- function(phi, theta){
+  f11 = function(x, theta){
+    aux = (2*cos(x)+2*theta)/(1+2*theta*cos(x)+theta^2)
+    aux = aux*aux/(4*pi)
+    aux
+  }
+  
+  f22 = function(x, phi){
+    aux = (-2*cos(x)+2*phi)/(1-2*phi*cos(x)+phi^2)
+    aux = aux*aux/(4*pi)
+    aux
+  }
+  
+  f12 = function(x, phi, theta){
+    aux1 = (-2*cos(x)+2*phi)/(1-2*phi*cos(x)+phi^2)
+    aux2 = (2*cos(x)+2*theta)/(1+2*theta*cos(x)+theta^2)
+    aux = aux1*aux2/(4*pi)
+    aux
+  }
+  
+  G11 = integrate(f11, lower = -pi, upper = pi, theta = theta)$value
+  1/(1-theta^2)
+  
+  G22 = integrate(f22, lower = -pi, upper = pi, phi = phi)$value
+  1/(1-phi^2)
+  
+  G12 = G21 = integrate(f12, lower = -pi, upper = pi, phi = phi, theta = theta)$value
+  -1/(1+phi*theta)
+  
+  G = matrix(c(G22,G12,G21,G11), ncol = 2, byrow = T)
+  solve(G)
+}
+
+var.coef.arma11(phi = 0.5, theta = 0.8)
+
+## Aplicación Malleco:
+n = length(X)
+
+## AR(1)
+fit <- forecast::Arima(X-mean(X), order = c(1,0,0), include.mean = F)
+fit
+
+## coef
+phi <- fit$coef[1]
+## s.e.
+se <- sqrt((1-phi^2)/n)
+## z value
+Z0 = fit$coef[1]/se
+## p value
+2*(1-pnorm(abs(Z0)))
+
+## ARMA(1,1)
+fit <- forecast::Arima(X-mean(X), order = c(1,0,1), include.mean = F)
+fit
+
+## z value
+Z0 <- fit$coef/sqrt(diag(fit$var))
+## p value
+2*(1-pnorm(abs(Z0)))
+
+source("summary.arima.R")
+
+summary_arima(fit, fixed = c(NA,NA))
--- script_tarea2.R
+++ script_tarea2.R
@@ -18,36 +18,150 @@ datos <- datos %>%
   filter(!is.na(X540011_raw)) 
 
 datos <- datos %>%
-  filter(age_CE < 1890)
+  filter(age_CE < 1890) %>% 
+  select(age_CE,X540021_raw) %>% 
+  rename("tiempo" = age_CE, "ancho" = X540021_raw)
+
+str(datos)
+
+# Volviendo a variables temporales ----
+Xt <- ts(datos$ancho, start = 1721, frequency = 1)
+t <- as.numeric(time(Xt, start = 1721))
 
 # Gráfico de serie de tiempo ----
 datos %>%
-  ggplot(aes(x = age_CE, y = X540021_raw)) +
-  geom_line(color = "brown4")+
-  geom_hline(yintercept = mean(datos$X540021_raw), col = "blue3") +
-  labs(x = "Año", y = "Ancho anillo") +
+  ggplot(aes(x = tiempo, y = ancho)) +
+  geom_line(color = "brown4", lwd = 0.8)+
+  geom_hline(yintercept = mean(datos$ancho),
+             col = "darkgreen",lwd = 0.8) +
+  labs(x = "Año", y = "Ancho anillo [mm]", title = "Variación del ancho del anillo de pinos silvestres de Noruega en mm") +
   theme_bw()
+
+# Box-Plot ----
+
+box_plot = datos %>%
+  ggplot(aes(x = ancho, y = 0)) +
+  # geom_violin(color = "brown3") +
+  geom_boxplot(fill = "brown4",
+               width = 0.3,color = "darkgreen") +
+  ylim(c(-0.5, 0.5)) +
+  theme_bw() +
+  xlab("CO ppm") +
+  theme(axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        axis.title.y = element_blank(),
+        plot.title = element_text(size = 10,
+                                  #face = "bold",
+                                  color = "black",
+                                  hjust = 0.5),
+        plot.subtitle = element_text(size = 9,
+                                     #face = "bold",
+                                     color = "black",
+                                     hjust = 0.5),
+        axis.title.x = element_text(size = 8)) +
+  labs(title = "CAMBIAR ESTA WEA",
+       subtitle = "ESTE TMB")
+
+box_plot
 
 # ACF y PACF ----
 
 acf(datos$X540021_raw, main = "ACF")
 pacf(datos$X540021_raw, main = "PACF")
 
-# Volviendo a variables temporales ----
-Xt <- ts(datos$X540021_raw, start = 1721, frequency = 1)
-t <- as.numeric(time(Xt, start = 1721)) # ayuda no empieza en 1721 aa
+# ACF base
+acf <- acf(Xt, plot = FALSE)
+acf_data <- data.frame(Lag = acf$lag,
+                       ACF = acf$acf)
+
+# PACF base
+pacf <- pacf(Xt, plot = FALSE)
+pacf_data <- data.frame(Lag = pacf$lag,
+                        PACF = pacf$acf)
+
+n = nrow(datos)
+
+ACF = ggplot(acf_data, aes(x = Lag, y = ACF)) +
+  geom_hline(yintercept = 0,
+             linetype = "dashed") + 
+  geom_hline(yintercept = 1.96/sqrt(n),
+             linetype = "dashed",
+             col = "darkgreen") + 
+  geom_hline(yintercept = -1.96/sqrt(n),
+             linetype = "dashed",
+             col = "darkgreen") + 
+  geom_segment(aes(xend = Lag,
+                   yend = 0),
+               color = "brown",
+               size = 1) + # Líneas verticales
+  geom_point(size = 2.5,
+             shape = 17,
+             col = "darkgreen") + # Puntos de color en el extremo de las líneas
+  labs(x = "Lag",
+       y = "ACF",
+       title = "Gráfico de Autocorrelación de los datos") +
+  theme_bw() +
+  theme(axis.text.y = element_text(size = 8),
+        # axis.ticks.y = element_blank(),
+        axis.title.y = element_text(size = 8),
+        plot.title = element_text(size = 10,
+                                  #face = "bold",
+                                  color = "black",
+                                  hjust = 0.5),
+        axis.title.x = element_text(size = 8),
+        axis.text.x = element_text(size = 8))
+
+PACF = pacf_data %>%
+  ggplot(aes(x = Lag, y = PACF)) +
+  geom_hline(yintercept = 0,
+             linetype = "dashed") + 
+  geom_hline(yintercept = 1.96/sqrt(n),
+             linetype = "dashed",
+             col = "darkgreen") + 
+  geom_hline(yintercept = -1.96/sqrt(n),
+             linetype = "dashed",
+             col = "darkgreen") + 
+  geom_segment(aes(xend = Lag,
+                   yend = 0),
+               color = "brown",
+               size = 1) + # Líneas verticales
+  geom_point(size = 2.5,
+             shape = 17,
+             col = "darkgreen") + # Puntos de color en el extremo de las líneas
+  labs(x = "Lag",
+       y = "ACF",
+       title = "Gráfico de Autocorrelación Parcial de los datos") +
+  theme_bw() +
+  theme(axis.text.y = element_text(size = 8),
+        # axis.ticks.y = element_blank(),
+        axis.title.y = element_text(size = 8),
+        plot.title = element_text(size = 10,
+                                  #face = "bold",
+                                  color = "black",
+                                  hjust = 0.5),
+        axis.title.x = element_text(size = 8),
+        axis.text.x = element_text(size = 8))
+
+
+PACF
+ACF
+
+# ARIMA ----
+
+auto.arima(Xt)
+## AR(1)
+fit <- forecast::Arima(Xt, order = c(0,1,1), include.mean = F)
+
 
 # Tests ----
-ks.test(Xt,"pnorm") # Rechaza normalidad y existen "ties"
-bptest(Xt ~ t) # no son homocedasticos
 
-lambda <- forecast::BoxCox.lambda(Xt, method = "guerrero")
+shapiro.test(fit$res) # son normales 
+bptest(fit$res ~ t) # son homocedasticos
 
-fit <- forecast::auto.arima(Xt, lambda = lambda)
-TS.diag(fit$res)
-salida.arima(Xt, fit)
+TS.diag(fit$res) # pasa test de blacura
+salida.arima(Xt, fit) # significancia de los coeficientes
 
-adf.test(resid(fit)) # No estacionaridad
+adf.test(fit$res) # SON ESTACIONARIOS
 
 acf(Xt) 
 pacf(Xt)
